48b129c90bedb62fee500afe641f02da5baa61c3
Espen Aa Ljosland <espen@ljosland.com>
2023-02-06 20:22:20 +0100

    1.1.0 - Migrated to SDKv3

--- drivers/common/ESPurnaDriver.js
+++ drivers/common/ESPurnaDriver.js
@@ -9,12 +9,12 @@ class ESPurnaDriver extends Homey.Driver {
 		this.log('Espurna driver driver has been initialized');
 	}
 
-    onPair( socket ) {
-        socket.on('configure', (device, callback) => {
-			console.log( device );
+    onPair( session ) {
+        session.setHandler('configure', async (device) => {
+            console.log( device );
 
-            http( `http://${device.settings.ip_address}/apis?apikey=${device.settings.apikey}`).then(function (result) {
-            	if(result.response.statusCode === 200) {
+            return http( `http://${device.settings.ip_address}/apis?apikey=${device.settings.apikey}`).then(function (result) {
+                if(result.response.statusCode === 200) {
                     console.log('Code: ' + result.response.statusCode);
                     console.log('Response: ' + result.data);
 
@@ -22,7 +22,7 @@ class ESPurnaDriver extends Homey.Driver {
                     if(/^relay\/0 /m.test(result.data))
                         capabilities.push("onoff");
                     if(/^voltage /m.test(result.data))
-                    	capabilities.push("measure_voltage");
+                        capabilities.push("measure_voltage");
                     if(/^current /m.test(result.data))
                         capabilities.push("measure_current");
                     if(/^power /m.test(result.data))
@@ -31,17 +31,17 @@ class ESPurnaDriver extends Homey.Driver {
                         capabilities.push("meter_power");
 
                     device.capabilities = capabilities;
-                    callback(null, device);
-				}
-            	else if(result.response.statusCode === 403)
-            		callback(new Error('Wrong API key?'));
-            	else
-            		callback(new Error("Could not connect. Verify IP and API key"));
+                    return device
+                }
+                else if(result.response.statusCode === 403)
+                    throw new Error('Wrong API key?');
+                else
+                    throw new Error("Could not connect. Verify IP and API key");
             }).catch(err =>
-			{
-				console.error(err);
-				callback(err);
-			});
+            {
+                console.error(err);
+                throw err;
+            });
         });
     }
 }
